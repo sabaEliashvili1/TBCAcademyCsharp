5ce10aab91442124a594fe847742e33745a2236b
sabaEliashvili1 <135349993+sabaEliashvili1@users.noreply.github.com>
2023-06-05 16:51:41 +0400

    Add files via upload

---
+++ Assigment 1/Task 2/Task 2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("What is current temperature?");
+int.TryParse(Console.ReadLine(),out int input);
+if(input < 0)
+{
+    Console.WriteLine("Freezing");
+}
+else if(input >= 0 && input <= 30)
+{
+    Console.WriteLine("Good Weather");
+}
+else if(input > 30)
+{
+    Console.WriteLine("Hot");
+}
+else
+{
+    Console.WriteLine("Wrong Input!!");
+}
---
+++ Assigment 1/Task 3/Task 3/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("What is your Play?(r,p,s)");
+var input = Console.ReadLine();
+var rand = new Random().Next(0,3);
+
+if (input == "r")
+{
+    if (rand == 0) { Console.WriteLine("Computer Played Rock! \n It's a Tie!"); }
+    else if (rand == 1) { Console.WriteLine("Computer Played Paper! \n You Lose!"); }
+    else if (rand == 2) { Console.WriteLine("Computer Played Scissors! \nYou Win!"); }
+}
+else if (input == "p")
+{
+    if (rand == 0) { Console.WriteLine("Computer Played Rock! \n You Win!"); }
+    else if (rand == 1) { Console.WriteLine("Computer Played Paper! \n It's a Tie!"); }
+    else if (rand == 2) { Console.WriteLine("Computer Played Scissors! \nYou Lose!"); }
+}
+else if (input == "s")
+{
+    if (rand == 0) { Console.WriteLine("Computer Played Rock! \n You Lose!"); }
+    else if (rand == 1) { Console.WriteLine("Computer Played Paper! \n You Win!"); }
+    else if (rand == 2) { Console.WriteLine("Computer Played Scissors! \n It's a Tie!"); }
+}
+else
+{
+    Console.WriteLine("Wrong Input!!");
+}
