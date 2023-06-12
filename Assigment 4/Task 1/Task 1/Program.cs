94e05549646a7eea07f6ed0478ac310074b5f65b
sabaeliashvili1 <eliashvili408@gmail.com>
2023-06-12 20:08:15 +0400

    add asigment 2

---
+++ Assigment 4/Task 1/Task 1/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Write("Input Length: ");
+if (!(int.TryParse(Console.ReadLine(), out int N)))
+{
+    Console.WriteLine("Wrong Input\n");
+    return;
+}
+
+int[] array1 = new int[N];
+for (int i = 0; i < N; i++)
+{
+    while (true)
+    {
+        Console.Write("[" + i + "]");
+        if (!(int.TryParse(Console.ReadLine(), out int temp)))
+        {
+            Console.WriteLine("Wrong Input");
+            continue;
+        }
+        else
+        {
+            array1[i] = temp;
+            break;
+        }
+    }
+}
+Console.Write("Input Index: ");
+if (!int.TryParse(Console.ReadLine(), out int index))
+{
+    Console.WriteLine("Wrong Input\n");
+    return;
+}
+Console.WriteLine(AddNumb(array1,index));
+
+static int AddNumb(int[] arr, int index)
+{
+    var ans = 0;
+    var number = arr[index];
+
+    while (number > 0)
+    {
+        ans += number % 10;
+        number /= 10;
+    }
+    return ans;
+}
---
+++ Assigment 4/Task 2/Program.cs
@@ -0,0 +1,69 @@
+﻿var array = InputArray();
+var charater = "";
+while (true)
+{
+    Console.Write("Input symbol: ");
+    charater = Console.ReadLine();
+    if (charater == "" || charater.Length > 1)
+    {
+        Console.WriteLine("Wrong Input");
+    }
+    else
+    {
+        break;
+    }
+}
+charNum(array, charater[0]);
+
+
+
+char[] InputArray()
+{
+    
+    var N = 0;
+    while (true)
+    {
+        Console.Write("Input Length: ");
+        if (!(int.TryParse(Console.ReadLine(), out N)))
+        {
+            Console.WriteLine("Wrong Input\n");
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    char[] array1 = new char[N];
+    for (int i = 0; i < N; i++)
+    {
+        while (true)
+        {
+            Console.Write("[" + i + "]");
+            var temp = Console.ReadLine();
+            if (temp == "" || temp.Length > 1)
+            {
+                Console.WriteLine("Wrong Input");
+            }
+            else
+            {
+                array1[i] = temp[0];
+                break;
+            }
+        }
+    }
+    return array1;
+}
+
+void charNum(char[] arr, char symbol)
+{
+    var ammount = 0;
+    foreach (var item in arr)
+    {
+        if(item == symbol)
+        {
+            ++ammount;
+        }
+    }
+    Console.WriteLine("Symbol \"" + symbol + "\" appears " + ammount + " Times in array");
+}
